6862b551fafba83c308f0fff3e1422a17e5df031
Kathy Lau <imobilep501@gmail.com>
2016-09-13 21:57:51 -0400

    completed

---
+++ randomizer.py
@@ -0,0 +1,29 @@
+import random
+
+
+CLASSES =  {
+    4: [ 'Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson',
+         'Brian', 'Farhan', 'Janet', 'Harry', 'Kevin',
+         'Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth',
+         'Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhriaj',
+         'Amy', 'Arvind', 'Nobel', 'Angela', 'Edward',
+         'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina'],
+    8: ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki',
+        'Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda',
+        'Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vincent',
+        'Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel',
+        'Felix', 'Jacob', 'Bayle', 'Fortune', 'Gio',
+        'Kelly', 'William', 'Jordan', 'Haley', 'Henry'],
+    9: ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C',
+        'Joel', 'Steph', 'Xinhui', 'Richard', 'Misha',
+        'Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry',
+        'Michael', 'Stiven', 'Will',  'Olivia', 'Constantine',
+        'Jessica', 'Kate', 'Shamaul', 'Max', 'Sarah', 'Anthony L',
+        'Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']
+}
+
+def randomizer(pd_num):
+    if period_num in [4, 8, 9]:
+        return CLASSES[pd_num][random.randrange(len(CLASSES[pd_num]))]
+    else:
+        return "Not a valid period number"
